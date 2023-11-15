db7dbc855b48b34bce0c8aba3578a7d792a2b66e
Edemialem <136074891+Edemialem21@users.noreply.github.com>
2023-11-15 14:57:13 +0300

    function that prints depending on permission

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -4,7 +4,7 @@ int cant_open(char *file_path);
 int proc_file_commands(char *file_path, int *exe_ret);
 
 /**
- * cant_open - Functions If the file doesn't exist or lacks proper permissions, print
+ * cant_open - Functions If the file doesn't exist or lacks proper permissions
  * a cant open error.
  * @file_path: Path to the supposed file.
  *
