b2e277ceda2e82126e2f532a7e208f9fdee8ab59
merga wolde <49088728+merga-wolde@users.noreply.github.com>
2023-11-15 14:56:13 +0300

    getline

--- getline.c
+++ getline.c
@@ -6,13 +6,13 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 
 /**
  * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
+ * @ptr: A ptr to the memory previously allocated memory.
  * @old_size: The size in bytes of the allocated space for ptr.
  * @new_size: The size in bytes for the new memory block.
  *
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ *         Otherwise - a ptr to the reallocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
